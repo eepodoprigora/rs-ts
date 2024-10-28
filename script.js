b5b38d94dc2e367c2ef0e4e7a167cec39037c9c0
Евгения Подопригора <eepodoprigora@gmail.com>
2024-10-28 13:41:46 +0300

    price calculator

--- script.js
+++ script.js
@@ -1,58 +1,15 @@
-var WeatherSoundsApp = /** @class */ (function () {
-    function WeatherSoundsApp() {
-        this.currentSound = null;
-        this.audioElement = new Audio();
-        this.audioElement.loop = true;
-        this.initVolumeControl();
-        this.initButtons();
+var totalPrice = function (_a) {
+    var price = _a.price, discount = _a.discount, isInstallment = _a.isInstallment, months = _a.months;
+    var discountedPrice = price * (1 - discount / 100);
+    if (isInstallment && months > 0) {
+        return discountedPrice / months;
     }
-    WeatherSoundsApp.prototype.initVolumeControl = function () {
-        var _this = this;
-        var volumeControl = document.querySelector(".volume");
-        volumeControl.addEventListener("input", function () {
-            _this.audioElement.volume = parseFloat(volumeControl.value);
-        });
-        this.audioElement.volume = parseFloat(volumeControl.value);
-    };
-    WeatherSoundsApp.prototype.initButtons = function () {
-        var _this = this;
-        var buttons = document.querySelectorAll(".button");
-        buttons.forEach(function (button) {
-            button.addEventListener("click", function () {
-                var sound = button.dataset.sound;
-                _this.toggleSound(sound, button);
-            });
-        });
-    };
-    WeatherSoundsApp.prototype.toggleSound = function (sound, button) {
-        var buttons = document.querySelectorAll(".button");
-        buttons.forEach(function (item) {
-            item.classList.remove("playing");
-            item.classList.remove("pause");
-        });
-        if (this.currentSound === sound) {
-            if (this.audioElement.paused) {
-                this.audioElement.play();
-                button.classList.add("playing");
-            }
-            else {
-                this.audioElement.pause();
-                button.classList.add("pause");
-            }
-        }
-        else {
-            this.playSound(sound);
-            button.classList.add("playing");
-        }
-    };
-    WeatherSoundsApp.prototype.playSound = function (sound) {
-        this.audioElement.src = "assets/sounds/".concat(sound, ".mp3");
-        this.audioElement.play();
-        this.currentSound = sound;
-        document.body.className = sound;
-    };
-    return WeatherSoundsApp;
-}());
-document.addEventListener("DOMContentLoaded", function () {
-    new WeatherSoundsApp();
+    return discountedPrice;
+};
+var price = totalPrice({
+    price: 100000,
+    discount: 25,
+    isInstallment: true,
+    months: 12,
 });
+console.log(price); // 6250
--- script.ts
+++ script.ts
@@ -1,63 +1,29 @@
-type Sound = "summer" | "rain" | "winter";
-
-class WeatherSoundsApp {
-  private audioElement: HTMLAudioElement;
-  private currentSound: Sound | null = null;
-
-  constructor() {
-    this.audioElement = new Audio();
-    this.audioElement.loop = true;
-
-    this.initVolumeControl();
-    this.initButtons();
-  }
-  private initVolumeControl() {
-    const volumeControl = document.querySelector(".volume") as HTMLInputElement;
-
-    volumeControl.addEventListener("input", () => {
-      this.audioElement.volume = parseFloat(volumeControl.value);
-    });
-    this.audioElement.volume = parseFloat(volumeControl.value);
-  }
-  private initButtons() {
-    const buttons = document.querySelectorAll<HTMLButtonElement>(".button");
-    buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const sound = button.dataset.sound as Sound;
-        this.toggleSound(sound, button);
-      });
-    });
-  }
-
-  private toggleSound(sound: Sound, button: HTMLButtonElement) {
-    const buttons = document.querySelectorAll<HTMLButtonElement>(".button");
-    buttons.forEach((item) => {
-      item.classList.remove("playing");
-      item.classList.remove("pause");
-    });
-    if (this.currentSound === sound) {
-      if (this.audioElement.paused) {
-        this.audioElement.play();
-        button.classList.add("playing");
-      } else {
-        this.audioElement.pause();
-        button.classList.add("pause");
-      }
-    } else {
-      this.playSound(sound);
-      button.classList.add("playing");
-    }
-  }
-
-  private playSound(sound: Sound) {
-    this.audioElement.src = `assets/sounds/${sound}.mp3`;
-    this.audioElement.play();
-
-    this.currentSound = sound;
-    document.body.className = sound;
-  }
+interface PriceOptions {
+  price: number;
+  discount: number;
+  isInstallment: boolean;
+  months: number;
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  new WeatherSoundsApp();
+const totalPrice = ({
+  price,
+  discount,
+  isInstallment,
+  months,
+}: PriceOptions) => {
+  const discountedPrice = price * (1 - discount / 100);
+
+  if (isInstallment && months > 0) {
+    return discountedPrice / months;
+  }
+
+  return discountedPrice;
+};
+
+const price = totalPrice({
+  price: 100000,
+  discount: 25,
+  isInstallment: true,
+  months: 12,
 });
+console.log(price); // 6250
